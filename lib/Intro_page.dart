55a451d6e6812d72f742cea9c32ff5ffb694ab5f
Sam Victor <iamvictorsam@gmail.com>
2023-02-22 17:23:46 +0100

    added Home page

--- lib/Intro_page.dart
+++ lib/Intro_page.dart
@@ -21,7 +21,7 @@ class _IntroPageState extends State<IntroPage> {
   Future retrieveIntroPage() async {
     try {
       final url = Uri.parse(
-          "https://api.buttercms.com/v2/pages/intro/intro?auth_token=468dba86d8c24c33ee6b65bfb5939f1b91a75fab");
+          "https://api.buttercms.com/v2/pages/intro/intro-page?auth_token=468dba86d8c24c33ee6b65bfb5939f1b91a75fab");
       final response = await http.get(url);
       if (response.statusCode == 200) {
         var result = jsonDecode(response.body);
@@ -46,115 +46,130 @@ class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.33,
-            width: double.maxFinite,
-            decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.3),
-                image: DecorationImage(
-                  image: NetworkImage(data[0]['fields']['hero_image']),
-                  fit: BoxFit.cover,
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.6), BlendMode.darken),
-                )),
-            child: Column(
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
               children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                const ListTile(
-                  leading: FlutterLogo(
-                    size: 40,
-                  ),
-                  title: Text(
-                    'Flutter',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30),
-                  ),
-                ),
-                const Spacer(),
-                Center(
-                  child: Text(
-                    data[0]['fields']['hero_title'],
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30),
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Text(
-            data[0]['fields']['section_title'],
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              'Articles to help get you started with Flutter: How To\'s,  Basics, FAQs, and more..',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black87, fontSize: 18),
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: InkWell(
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const HomeScreen(),
-                  )),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 20),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.33,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.3),
+                      image: DecorationImage(
+                        image: NetworkImage(data[0]['fields']['hero_image']),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.6), BlendMode.darken),
+                      )),
                   child: Column(
                     children: [
-                      Row(children: const [
-                        CircleAvatar(
-                          backgroundColor: Colors.black,
-                          radius: 48,
-                          child: FlutterLogo(
-                            size: 48,
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const ListTile(
+                        leading: FlutterLogo(
+                          size: 40,
+                        ),
+                        title: Text(
+                          'Flutter',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Center(
+                          child: Text(
+                            data[0]['fields']['hero_title'],
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
                           ),
                         ),
-                        SizedBox(width: 20),
-                        Text(
-                          'How To\'s',
-                          style: TextStyle(color: Colors.black87, fontSize: 30),
-                        ),
-                      ]),
-                      const SizedBox(height: 20),
-                      const Text(
-                        'How to use the Flutter cli to install packages, use packages, write efficient flutter codes ... and more!',
-                        style: TextStyle(color: Colors.black87, fontSize: 18),
+                      ),
+                      const SizedBox(
+                        height: 40,
                       ),
                     ],
                   ),
                 ),
-              ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  data[0]['fields']['section_title'],
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 28),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    data[0]['fields']['section_desc'],
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Colors.black87, fontSize: 18),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                ...List.generate(
+                  data[0]['fields']['contents'].length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: InkWell(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomeScreen(),
+                          )),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 20),
+                          child: Column(
+                            children: [
+                              Row(children: [
+                                CircleAvatar(
+                                  backgroundColor: Colors.black,
+                                  backgroundImage: NetworkImage(data[0]
+                                          ['fields']['contents'][index]
+                                      ['content_logo']),
+                                  radius: 48,
+                                ),
+                                const SizedBox(width: 20),
+                                Text(
+                                  data[0]['fields']['contents'][index]
+                                      ['content_title'],
+                                  style: const TextStyle(
+                                      color: Colors.black87, fontSize: 30),
+                                ),
+                              ]),
+                              const SizedBox(height: 20),
+                              Text(
+                                data[0]['fields']['contents'][index]
+                                    ['content_desc'],
+                                style: const TextStyle(
+                                    color: Colors.black87, fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
